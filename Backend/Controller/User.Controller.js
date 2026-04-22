a01db239e1699dc02a57a736b45c82c17e98d0fd
jitnesh verma <jitneshverma@jitneshs-MacBook-Air.local>
2026-04-22 18:46:22 +0530

    change in code

--- Backend/Controller/User.Controller.js
+++ Backend/Controller/User.Controller.js
@@ -157,8 +157,9 @@ export const login = async(req, res) => {
 
         res.cookie("token", token, {
             httpOnly: true,
-            secure: true, // REQUIRED on HTTPS (Render uses HTTPS)
+            secure: true,
             sameSite: "None",
+            path: "/",
             maxAge: 24 * 60 * 60 * 1000,
         });
 
--- Backend/MiddleWare/userAutho.js
+++ Backend/MiddleWare/userAutho.js
@@ -5,6 +5,7 @@ export const authenticateUser = async(req, res, next) => {
 
     try {
         const token = req.cookies.token;
+        console.log(token);
 
         if (!token) {
             return res.status(401).json({ message: "Authentication token missing" });

dd904b94dd1c771a9e8be967b6594ca0aa553e1b
jitnesh verma <sonuvermavs143@gmail.com>
2026-04-22 12:18:57 +0530

    api change

--- Fronted/vite-project/src/Pages/OAuthSuccess.jsx
+++ Fronted/vite-project/src/Pages/OAuthSuccess.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { loginSuccess } from "../../Redux/authoSlice";
+const API_URL = import.meta.env.VITE_API_URL;
 
 function OAuthSuccess() {
   const [params] = useSearchParams();
@@ -16,7 +17,7 @@ function OAuthSuccess() {
       localStorage.setItem("token", token);
 
       // 🔥 Fetch user profile
-      fetch("http://localhost:3000/api/users/profile", {
+      fetch(`${API_URL}/api/users/profile`, {
         credentials: "include",
       })
         .then(res => res.json())

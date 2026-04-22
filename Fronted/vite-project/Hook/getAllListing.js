de0b9571452c36b9c21c51802b895e50f905bcdf
jitnesh verma <sonuvermavs143@gmail.com>
2026-04-22 12:41:52 +0530

    api change

--- Fronted/vite-project/Hook/getAllListing.js
+++ Fronted/vite-project/Hook/getAllListing.js
@@ -2,14 +2,14 @@ import axios from "axios";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { getListings } from "../Redux/Listing.js";
-
+const API_URL = import.meta.env.VITE_API_URL;
 
 const useFetchAllListing = () => {
     const dispatch = useDispatch();
 
     const fetchListings = async() => {
         try {
-            const res = await axios.get("http://localhost:3000/api/listing/all/listings", { withCredentials: true });
+            const res = await axios.get(`${API_URL}/api/listing/all/listings`, { withCredentials: true });
 
             if (res.data.success) {
                 dispatch(getListings(res.data.listings));
